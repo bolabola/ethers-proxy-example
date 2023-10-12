448cd2c9ae4920ed2bbbbcc60bb35e71c34f273c
kaliubuntu0206 <139627505+kaliubuntu0206@users.noreply.github.com>
2023-10-13 08:32:55 +0900

    Simplify example

--- index.js
+++ index.js
@@ -7,59 +7,7 @@ const { createProxy } = require('proxy');
 const ETH_RPC = 'https://eth.llamarpc.com';
 // Example that doesn't use proxy connections
 const BSC_RPC = 'https://binance.llamarpc.com';
-
-const HTTP_PROXY_PORT = 3128;
-const HTTP_PROXY_HOST = 'localhost';
-const HTTP_PROXY = `http://${HTTP_PROXY_HOST}:${HTTP_PROXY_PORT}`;
-
-/**
- * Define our own custom getUrl (typeof FetchGetUrlFunc) function
- *
- * See examples on
- * https://github.com/ethers-io/ethers.js/blob/main/src.ts/utils/geturl-browser.ts
- *
- * Documents:
- * https://docs.ethers.org/v6/api/utils/fetching/#FetchRequest_registerGetUrl
- * https://docs.ethers.org/v6/api/utils/fetching/#FetchGetUrlFunc
- */
-const getUrl = async (req, _signal) => {
-  // Inherited from https://github.com/ethers-io/ethers.js/blob/main/src.ts/utils/geturl-browser.ts
-  let signal;
-
-  if (_signal) {
-      const controller = new AbortController();
-      signal = controller.signal;
-      _signal.addListener(() => { controller.abort(); });
-  }
-
-  const init = {
-    method: req.method,
-    headers: req.headers,
-    body: req.body || undefined,
-    signal
-  };
-
-  // This is what we want
-  init.agent = new HttpsProxyAgent(HTTP_PROXY);
-
-  // Inherited from https://github.com/ethers-io/ethers.js/blob/main/src.ts/utils/geturl-browser.ts
-  const resp = await fetch(req.url, init);
-
-  const headers = {};
-  resp.headers.forEach((value, key) => {
-    headers[key.toLowerCase()] = value;
-  });
-
-  const respBody = await resp.arrayBuffer();
-  const body = (respBody == null) ? null: new Uint8Array(respBody);
-
-  return {
-    statusCode: resp.status,
-    statusMessage: resp.statusText,
-    headers,
-    body
-  };
-};
+const HTTP_PROXY = `http://localhost:3128`;
 
 /**
  * Define new FetchRequest used for a provider
@@ -67,7 +15,7 @@ const getUrl = async (req, _signal) => {
 // Assigning custom getUrl function will apply to all ethers v6 providers
 
 const fetchReq = new FetchRequest(ETH_RPC);
-fetchReq.getUrlFunc = getUrl;
+fetchReq.getUrlFunc = FetchRequest.createGetUrlFunc({ agent: new HttpsProxyAgent(HTTP_PROXY) });
 const provider = new JsonRpcProvider(fetchReq);
 const provider2 = new JsonRpcProvider(BSC_RPC);
 
